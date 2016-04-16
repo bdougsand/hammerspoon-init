a6f686271367507e340bd69ac44d1e9e1d689f34
Brian Sanders <bdougsand+cfb@gmail.com>
2016-04-16 09:58:39 -0400

    Tapping twice adjusts next window

--- init.lua
+++ init.lua
@@ -1,13 +1,67 @@
+local lastRun = 0
+
 function windowMove(wScale, hScale, xScale, yScale)
+  local now = hs.timer.secondsSinceEpoch()
   local window = hs.window.focusedWindow()
+
+  if window:isFullScreen() then
+    return
+  end
+
   local f = window:frame()
   local screenFrame = window:screen():frame()
 
-  f.x = screenFrame.w*xScale
-  f.y = screenFrame.h*yScale
-  f.w = screenFrame.w*wScale
-  f.h = screenFrame.h*hScale
-  window:setFrame(f)
+  local newX = screenFrame.w*xScale
+  local newY = screenFrame.h*yScale
+  local newW = screenFrame.w*wScale
+  local newH = screenFrame.h*hScale
+  local dt = now - lastRun
+  lastRun = now
+
+  if dt >= 0.5 then
+    f.x = newX
+    f.y = newY
+    f.w = newW
+    f.h = newH
+    window:setFrame(f)
+  else
+    local screenId = window:screen():id()
+    local windows = hs.window.orderedWindows()
+    -- local i = 2
+    -- window = windows[i]
+    -- while window and window:screen():id() ~= screenId do
+    --   i = i + 1
+    --   window = windows[i]
+    -- end
+    window = windows[2]
+
+    if window then
+      if wScale < 1 then
+        newW = (1 - wScale)*screenFrame.w
+        if xScale == 0 then
+          newX = wScale*screenFrame.w
+        else
+          newX = 0
+        end
+      elseif yScale < 1 then
+        newH = (1 - hScale)*screenFrame.h
+        if yScale == 0 then
+          newY = hScale*screenFrame.h
+        else
+          newY = 0
+        end
+      else
+        return
+      end
+
+      f = window:frame()
+      f.x = newX
+      f.y = newY
+      f.w = newW
+      f.h = newH
+      window:setFrame(f)
+    end
+  end
 end
 
 function windowLeft()
